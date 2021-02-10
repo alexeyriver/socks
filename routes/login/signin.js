2df923aa48a7fdf727f40acd40c849f1772076f4
maikltim <timofeevms@mail.ru>
2021-02-10 14:12:58 +0200

    1512

--- routes/login/signin.js
+++ routes/login/signin.js
@@ -1,9 +1,8 @@
-
-
-
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user') 
 
-router.get('/',(req,res)=>{
+router.get('/signin',(req,res)=>{
   
   /////  вход  пользователя
   //// если такого пользователя нет, выводим ошибку
@@ -14,8 +13,38 @@ router.get('/',(req,res)=>{
 
 
 
-  res.render('index')
+  res.render('signin')
 })
+
+
+router.post('/signin', (req, res) => {
+  const { email, password } = req.body
+
+  const candidate = await User.findOne({email})
+  if(candidate) {
+    if(bcrypt,bcrypt.compareSync(password, candidate.password)) {
+        req.session.user = candidate.name // ???
+        res.status(200).json({
+            success: true,
+            message: 'User in'
+        })
+    } else {
+        res.status(404).json({
+            success: false,
+            message: 'User donot found'
+        })
+    }
+
+} else {
+    res.status(404).json({
+        success: false,
+        message: 'User donot found'
+    })
+}
+
+
+})
+
 
 
 module.exports= router
--- routes/login/signup.js
+++ routes/login/signup.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const User = require('../models/user') 
 
 // registartion
 
