ac64c72f375b45d20f12845dcd58f8340dac1bc2
unknown <alexeyriver@gmail.com>
2021-02-10 14:29:11 +0300

    release1430

---
+++ routes/login/logout.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+
+router.get('/',(req,res)=>{
+  
+  /////  выход пользователяя 
+  //// удаление куков и сесиии
+  ///// редирект на мэйн
+ 
+
+
+
+
+
+  res.render('index')
+})
+
+
+module.exports= router
---
+++ routes/login/signup.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+
+router.get('/',(req,res)=>{
+  
+  /////  регистрация пользователя
+  //// проверка имени пользователя на уникальность
+  //// 
+  ///// редирект на мэйн
+ 
+
+
+
+
+
+  res.render('index')
+})
+
+
+module.exports= router
