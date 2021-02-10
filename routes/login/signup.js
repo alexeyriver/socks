c5baa19eec94962956a279a4342e0e883fc1fea3
maikltim <timofeevms@mail.ru>
2021-02-10 13:57:31 +0200

    1457

--- routes/login/signup.js
+++ routes/login/signup.js
@@ -1,19 +1,37 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
 
-router.get('/',(req,res)=>{
-  
+// registartion
+
+router.get('/signup',(req,res)=>{
+
   /////  регистрация пользователя
   //// проверка имени пользователя на уникальность
   //// 
   ///// редирект на мэйн
- 
-
-
-
-
-
-  res.render('index')
+  res.render('signup')
 })
+
+
+router.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body
+  const salt = bcrypt.genSaltSync(10)
+  const hashPassword = bcrypt.hashSync(password, salt) 
+  const user = new User({
+    name,
+    email,
+    password: hashPassword
+  })
+
+  req.session.name = name
+  await user.save()
+
+  res.status(201).json({
+    success: true,
+    message: 'User done'
+})
+})
+
 
 
 module.exports= router
